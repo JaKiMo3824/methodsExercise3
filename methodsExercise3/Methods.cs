00c0394b80f1972bccabc928133728f84bfbc03f
JaKiMo3824 <jacob.moullet@gmail.com>
2018-08-10 13:12:09 -0700

    methodsExercise3
    exercises 1-6

---
+++ methodsExercise3/Methods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsExercise3
+{
+    class Methods
+    {
+        public static int Method1(int num1)
+        {
+            int res1 = num1 / 2;
+            return res1;
+        }
+
+        public static decimal Method2(decimal num2)
+        {
+            decimal res2 = num2 * 0.5m;
+            return res2;
+        }
+
+        public static string Method3(string num3)
+        {
+            int number3 = Convert.ToInt32(num3);
+            int res3 = number3 + 10;
+            string reso3 = res3.ToString();
+            return reso3;
+
+        }
+    }
+}
--- methodsExercise3/Program.cs
+++ methodsExercise3/Program.cs
@@ -10,6 +10,17 @@ namespace methodsExercise3
     {
         static void Main(string[] args)
         {
+            int result1 = Methods.Method1(12);
+            Console.WriteLine(result1);
+
+            decimal result2 = Methods.Method2(50m);
+            Console.WriteLine(result2);
+
+            string result3 = Methods.Method3("12");
+            Console.WriteLine(result3);
+
+            Console.ReadLine();
+
         }
     }
 }
